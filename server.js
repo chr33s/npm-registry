38723a8d0a0bd64fb9399bdac7d155bd026a2b91
Chris <chr33s@icloud.com>
2017-08-01 16:12:59 +0200

    remove: express etag

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express')
 const { npm } = require('./')
 
 const app = express()
+app.set('etag', false)
 app.use(bodyParser.json({ strict: false })) // npm dist-tags add <package>@<version> <tag> return '"0.0.1"'
 app.use(npm)
 app.listen(8080)
