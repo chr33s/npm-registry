37970eb382975794ff6bb34663529a063c70b97a
Chris <chr33s@icloud.com>
2017-08-04 20:00:56 +0200

    fix: team

--- routes/team/create.js
+++ routes/team/create.js
@@ -3,7 +3,11 @@
 const { data } = require('../../lib/storage')
 
 const create = (req, res) => (
-  data('save', ['Team', req.body.name], { name: req.body.name, users: [], permissions: {} })
+  data('save', ['Team', req.body.name], {
+    name: req.body.name,
+    users: [req.params.scope],
+    permissions: {}
+  })
     .then(() => data('get', ['Team', req.body.name]))
     .then(team => ({
       status: 200,
--- routes/team/ls.js
+++ routes/team/ls.js
@@ -3,7 +3,7 @@
 const { data } = require('../../lib/storage')
 
 const ls = (req, res) => (
-  data('get', ['Team', req.params.name])
+  data('get', ['Team', req.params.team])
     .then(team => ({
       status: 200,
       body: team.users
