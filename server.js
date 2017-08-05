d4ca570274edfc4196be9afd124fbb343fcb0220
Chris <chr33s@icloud.com>
2017-08-05 12:04:36 +0200

    deploy: app engine [!cloud function compatibility]

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const { npm } = require('./')
 
 const app = express()
 app.set('etag', false)
+app.use((req, res, next) => { // healthcheck
+  if (!(/^\/_ah\/.*/.test(req.path))) return next()
+  res.status(200).send('OK')
+})
 app.use(bodyParser.json({ strict: false })) // npm dist-tags add <package>@<version> <tag> return '"0.0.1"'
 app.use(npm)
-app.listen(8080)
+app.listen(process.env.PORT || process.env.NODE_PORT || 8080)
